c3bb8b67c8d64fff67badd24f247f38fdf9bdaac
zach-morris-text <oneblondhuman@gmail.com>
2024-10-21 05:25:59 -0400

    Build play/pause toggle functionality

--- scripts.js
+++ scripts.js
@@ -20,3 +20,27 @@ function loadSong(song) {
     audio.src = `music/${song}.mp3`
     cover.src = `images/${song}.jpg`
 }
+
+function playSong() {
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+}
+
+function pauseSong() {
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+}
+
+
+//Event Listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if(isPlaying) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+})
