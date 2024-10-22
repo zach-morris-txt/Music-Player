452a77b08a39216503d37f0d34c8556590ee4f3b
zach-morris-text <oneblondhuman@gmail.com>
2024-10-21 22:27:59 -0400

    Build next and prev functionality

--- scripts.js
+++ scripts.js
@@ -38,6 +38,23 @@ function pauseSong() {
     audio.pause()
 }
 
+function prevSong() {
+    songIndex--
+    if(songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+function nextSong() {
+    songIndex++
+    if(songIndex > songs.length -1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
 
 //Event Listeners
 playBtn.addEventListener('click', () => {
@@ -49,3 +66,6 @@ playBtn.addEventListener('click', () => {
         playSong()
     }
 })
+
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
