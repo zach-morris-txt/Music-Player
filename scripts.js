ccd2f7ae005a0752455a691c3700e51fb0632766
zach-morris-text <oneblondhuman@gmail.com>
2024-10-21 22:46:19 -0400

    Make audio jump next upon ending

--- scripts.js
+++ scripts.js
@@ -92,3 +92,5 @@ nextBtn.addEventListener('click', nextSong)
 audio.addEventListener('timeupdate', updateProgress)
 
 progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
