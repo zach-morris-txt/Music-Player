5a2a325e8c986d1f427c21adb58430cdcd688809
zach-morris-text <oneblondhuman@gmail.com>
2024-10-21 22:43:37 -0400

    Build audio jump-to functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//DOM
 const musicContainer = document.getElementById('music-container')
 const playBtn = document.getElementById('play')
 const prevBtn = document.getElementById('prev')
@@ -9,13 +10,16 @@ const progressContainer = document.getElementById('progress-container')
 const title = document.getElementById('title')
 const cover = document.getElementById('cover')
 
-
 const songs = ['song1', 'song2', 'song3']
 
 let songIndex = 0
+
+
 //Intial load song DOM
 loadSong(songs[songIndex])
 
+
+//Functions
 function loadSong(song) {
     title.innerText = song
     audio.src = `music/${song}.mp3`
@@ -56,6 +60,20 @@ function nextSong() {
     playSong()
 }
 
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+    audio.currentTime = (clickX / width) * duration
+}
+
+
 //Event Listeners
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play')
@@ -68,4 +86,9 @@ playBtn.addEventListener('click', () => {
 })
 
 prevBtn.addEventListener('click', prevSong)
+
 nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
